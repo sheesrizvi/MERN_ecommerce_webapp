54482cea450878e06cd32aa27f6998fe8f69a40b
Izhar Rizvi <you@example.com>
2021-05-20 16:35:50 +0530

    Update to pagenation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,7 @@ import Product from "../models/productModel.js"
 
 const getProducts = asyncHandler(async (req, res) => {
 
-    const pageSize = 2
+    const pageSize = 12
     const page = Number(req.query.pageNumber) || 1
 
     const keyword = req.query.keyword ? {
